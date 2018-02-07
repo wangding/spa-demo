51acff172a42d46cc197da5a97e818f52a2f59cb
wangding <408542507@qq.com>
2018-02-07 18:19:42 +0800

    feat: 试验了按钮样式的 Demo

---
+++ 01-html-widget/03-button.js
@@ -0,0 +1,56 @@
+var $btnEnabled,       // Enabled 按钮 
+    $btnDisabled,      // Disabled 按钮
+    $btnPlay,          // 图片按钮
+    $btnRound,         // 圆角按钮
+    timer = 0,         // 定时器
+    i;                 // 计数器
+
+$(function() {
+  $btnEnabled = $('#btnEnabled');
+  $btnDisabled = $('#btnDisabled');
+  $btnPlay = $('#btnPlay');
+  $btnRound = $('#btnRound');
+  init();
+
+  function init() {
+    i = 9;
+    $btnDisabled.val('同意 (' + i + ' s)');
+    $btnDisabled.attr('disabled', 'disabled');
+    $('#status').text('无');
+
+    if(timer !== 0) return;
+
+    timer = window.setInterval(function() {
+      $btnDisabled.val('同意 (' + i-- + ' s)');
+      if(i === -1) {
+        window.clearInterval(timer);
+        timer = 0;
+        $btnDisabled.val('同意');
+        $btnDisabled.removeAttr('disabled');
+      }
+    }, 1000);
+  }
+
+  $btnEnabled.click(function() {
+    alert('就知道你会点我！');
+  });
+
+  $btnDisabled.click(function() {
+    alert('你终于同意了！');
+  });
+
+  $btnPlay.click(function(){
+    $('#status').text('播放');
+  });
+
+  $('#btnPause').click(function(){
+    $('#status').text('暂停');
+  });
+
+  $('#btnStop').click(function(){
+    $('#status').text('关闭');
+  });
+
+  $btnRound.click(init);
+});
+
