b8759857358d743ef6a5c28e7dbe8fc78804c572
wangding <408542507@qq.com>
2018-02-09 17:31:08 +0800

    feat: 个人资料 Demo

---
+++ 01-html-widget/11-textbox.js
@@ -0,0 +1,13 @@
+$(function(){
+  $('#btnOk').click(function(){
+    var msg = '';
+    msg += '姓名：' + $('#name').val() + '\n';
+    msg += '学历：' + $('#study').val() + '\n';
+    msg += '生日：' + $('#birth').val() + '\n';
+    msg += '手机：' + $('#tell').val() + '\n';
+    msg += '邮箱：' + $('#email').val() + '\n';
+    msg += '网址：' + $('#url').val() + '\n';
+    msg += '简介：' + $('textarea').val();
+    alert(msg);
+  });
+});
