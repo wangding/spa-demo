ed4c7838c1e5789d5a2938f1fa3f397f722d4a6a
wangding <408542507@qq.com>
2018-02-09 15:23:28 +0800

    feat: 对定时器按钮组件化
    只实现了 DOM 绘制，用创建 DOM 节点的方式

---
+++ 03-ui-component/01-button/02-button.js
@@ -0,0 +1,30 @@
+var $btn, 
+  i = 9,
+  timer;
+
+$(function() {
+  /*
+  var $container = $('div.box');
+  $('<input type="button" value="同意">').appendTo($container);
+  */
+  
+  var $button = $('<input type="button" value="同意">');
+  $('div.box').append($button);
+
+  $btn = $('input[type="button"]');
+  $btn.val('同意 (' + i + ' s)');
+  $btn.attr('disabled', 'disabled');
+  
+  timer = window.setInterval(function() {
+    $btn.val('同意 (' + i-- + ' s)');
+    if(i === -1) {
+      window.clearInterval(timer);
+      $btn.val('同意');
+      $btn.removeAttr('disabled');
+    }
+  }, 1000);
+
+  $btn.click(function() {
+    alert('就知道你会同意的！');
+  });
+});
