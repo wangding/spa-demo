65806cfad4e2e5d4e2580b3dc78eb02b90a0cd73
wangding <408542507@qq.com>
2018-02-11 14:50:50 +0800

    feat: 对定时器按钮组件化
    实现了组件参数化

---
+++ 03-ui-component/01-button/06-button.js
@@ -0,0 +1,30 @@
+var $timerButton = (function() {
+  var init;
+  var $btn, 
+    i = 9,
+    html = '<input class="timer-button" type="button" value="同意">',
+    timer;
+
+  init = function(config) {
+    i = config.timer;
+    $(config.container).html(html);
+    $btn = $('input[type="button"]');
+    $btn.val(config.text + ' (' + i + ' s)');
+    $btn.attr('disabled', 'disabled');
+
+    timer = window.setInterval(function() {
+      $btn.val(config.text + ' (' + i-- + ' s)');
+      if(i === -1) {
+        window.clearInterval(timer);
+        $btn.val(config.text);
+        $btn.removeAttr('disabled');
+      }
+    }, 1000);
+    
+    $btn.click(function() {
+      alert('就知道你会同意的！');
+    });
+  };
+
+  return {'init': init};
+}());
