61d00915ea47eccee85e69574782867ac042cee3
wangding <408542507@qq.com>
2018-02-11 18:19:21 +0800

    feat:  对定时器按钮组件化
    实现组件实例化，可以在一个页面上构建多个组件

---
+++ 03-ui-component/01-button/08-button.js
@@ -0,0 +1,42 @@
+var TimerButton = function(config) {
+  var $btn, 
+    i = config.duration,
+    html = '<input class="timer-button" type="button" value="">',
+    enabled = config.enabled,
+    timer;
+
+  $(config.container).html(html);
+  $btn = $(config.container + '>input.timer-button');
+
+  ($btn.reset = function(enabledBtn) {
+    enabled = (enabledBtn === undefined) ? enabled : enabledBtn;
+    $btn.val(btnText());
+    if(enabled) return;
+    
+    $btn.attr('disabled', 'disabled');
+
+    timer = window.setInterval(function() {
+      i--;
+      $btn.val(btnText());
+      if(i === 0) enableBtn();
+    }, 1000);
+  })();
+
+  function enableBtn() {
+    enabled = true;
+    i = config.duration;
+    window.clearInterval(timer);
+    $btn.val(btnText());
+    $btn.removeAttr('disabled');
+  }
+
+  function btnText() {
+    return enabled ? config.text : config.text + ' (' + i + ' s)';
+  }
+
+  $btn.click(function() {
+    $btn.trigger('timer-button-click');
+  });
+
+  return $btn;
+};
