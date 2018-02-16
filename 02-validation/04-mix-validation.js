e214a93f13933ab0ee9cb2870200d79cae69ee1b
liujinmenghaoren <1325479537@qq.com>
2018-02-16 14:55:02 +0800

    feat: 个人资料2 Demo feat: H5 混合校验

---
+++ 02-validation/04-mix-validation.js
@@ -0,0 +1,14 @@
+$(function(){
+  $('form').submit(function(e) {
+    var msg = '';
+    msg += '姓名：' + $('#name').val() + '\n';
+    msg += '学历：' + $('#study').val() + '\n';
+    msg += '生日：' + $('#birth').val() + '\n';
+    msg += '手机：' + $('#tell').val() + '\n';
+    msg += '邮箱：' + $('#email').val() + '\n';
+    msg += '网址：' + $('#url').val() + '\n';
+    msg += '简介：' + $('textarea').val();
+    alert(msg);
+    e.preventDefault();
+  });
+});
