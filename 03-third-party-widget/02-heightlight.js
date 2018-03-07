66311717de62eedeec423ed6f27c03987e190a8d
wangding <408542507@qq.com>
2018-03-07 09:40:07 +0800

    feat: 语法高亮提升了 IDE

--- 03-third-party-widget/02-heightlight.js
+++ 03-third-party-widget/02-heightlight.js
@@ -1,13 +1,23 @@
 $(function() {
-  //hljs.initHighlightingOnLoad();
+  var editor = new Behave({
+    textarea: $('.main textarea').get(0),
+    replaceTab: true,
+    softTabs: true,
+    tabSize: 2,
+    autoOpen: true,
+    overwrite: true,
+    autoStrip: true,
+    autoIndent: true,
+    fence: false
+  });
 
   $('input[type="button"]').click(function() {
-    var $code = $('<div><pre><code class="javascript"></code></pre></div>'),
+    var $code = $('<div><pre class="javascript"></pre></div>'),
       $txt = $('.main textarea');
 
     if($txt.val() !== '') {
-      $code.find('code').html($txt.val());
-      hljs.highlightBlock($code.find('pre code').get(0));
+      $code.find('pre').html($txt.val());
+      hljs.highlightBlock($code.find('pre').get(0));
       $('.main').append($code);
     }
   });
