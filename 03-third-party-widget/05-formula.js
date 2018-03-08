0488096d91ef1194700b1aa9c2f76fcf14f248c1
wangding <408542507@qq.com>
2018-03-08 10:20:35 +0800

    feat: 数据公式编辑控件 Demo

---
+++ 03-third-party-widget/05-formula.js
@@ -0,0 +1,22 @@
+$(function() {
+  var MQ = MathQuill.getInterface(2);
+  var formulaSpan = $('#formula').get(0);
+  var formula = '';
+
+  var formulaField = MQ.MathField(formulaSpan, {
+    handlers: {
+      edit: function() {
+        var enteredMath = formulaField.latex();     
+        formula = enteredMath;
+      }
+    }
+  });
+
+  $('input[type="button"]').click(function() {
+    var $formula = $('<p><span class="formula"></span></p>');
+    
+    $formula.find('.formula').html(formula);
+    MQ.StaticMath($formula.find('.formula').get(0));
+    $('.main').append($formula);
+  });
+});
