f1c541f595c48e56bc4f6da8aff5618fbfe1db0d
wangding <408542507@qq.com>
2018-03-13 17:09:52 +0800

    fix: 修复了矩形计算器字段验证的问题
    矩形计算器字段验证不通过时，强行点击计算按钮，仍能计算

--- 00-first-app/rectangle.js
+++ 00-first-app/rectangle.js
@@ -1,40 +1,46 @@
 $(function() {
-  var $width = $('#rectangle-width');
-  var $height = $('#rectangle-height');
-  var $calc = $('#rectangle-calc');
+  var $width = $('#rectangle-width'),
+    $height = $('#rectangle-height'),
+    $calc = $('#rectangle-calc'),
+    blnValid = false;
 
   $calc.click(function() {
-    var width = $width.val();
-    var height = $height.val();
+    if(!blnValid) return;
 
-    $('#rectangle-perimeter').val(perimeter(width, height));
-    $('#rectangle-area').val(area(width, height));
+    var width = Number($width.val()),
+      height = Number($height.val()),
+      p = roundFractional(width * 2 + height * 2, 2),
+      a = roundFractional(width * height, 2);
+
+    $('#rectangle-perimeter').val(p);
+    $('#rectangle-area').val(a);
   });
 
   $width.focusout(function() {
-    validate('#rectangle-width');
+    blnValid = validate('#rectangle-width');
   });
   
   $height.focusout(function() {
-    validate('#rectangle-height');
+    blnValid = validate('#rectangle-height');
   });
-
-  function perimeter(x, y) {
-    var lenX = (x.split('.').length === 2) ? x.split('.')[1].length : 0,
-        lenY = (y.split('.').length === 2) ? y.split('.')[1].length : 0;
-
-    var max = Math.max(lenX, lenY);
-    return (x * (10 ** max) + y * (10 ** max)) * 2 / (10 ** max);
-  }
-
-  function area(x, y) {
-    var lenX = (x.split('.').length === 2) ? x.split('.')[1].length : 0,
-        lenY = (y.split('.').length === 2) ? y.split('.')[1].length : 0;
-
-    var max = Math.max(lenX, lenY);
-    return (x * (10 ** max)) * (y * (10 ** max)) / (10 ** (2 * max));
-  }
   
+  /**
+   * 小数点后面保留第 n 位
+   * 
+   * @param x 做近似处理的数
+   * @param n 小数点后第 n 位
+   * @returns 近似处理后的数 
+   */
+  function roundFractional(x, n) {
+    return Math.round(x * Math.pow(10, n)) / Math.pow(10, n);
+  }
+
+  /**
+   * 对字段进行数据合法性校验
+   *
+   * @param field 字段的 id
+   * @returns boolean 验证通过为 true，验证不通过为 false
+   */
   function validate(field) {
     var $data = $(field),
       $message = $(field + '-validate'),
@@ -43,23 +49,23 @@ $(function() {
     if($data.val() === '') {
       $message.html(label + '不能为空！');
       $data.select();
-      return;
+      return false;
     }
 
     if(!/^-?(0|[1-9]\d*)(\.\d*)?([eE][+-]?\d+)?$/.test($data.val())) {
       $message.html(label + '必须是数值');
       $data.select();
-      return;
+      return false;
     }
 
     if(window.Number($data.val()) < 0) {
       $message.html(label + '必须大于零');
       $data.select();
-      return;
+      return false;
     }
 
     $message.html('');
+    return true;
   }
 });
-
 
