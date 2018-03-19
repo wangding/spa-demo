8fc611075e5d245a9ba5ed5741cff798418c71cd
wangding <408542507@qq.com>
2018-03-19 11:04:05 +0800

    feat: 实现了 requirejs 对定时器按钮的封装

---
+++ 04-ui-component/01-button/10-index.js
@@ -0,0 +1,31 @@
+requirejs.config({
+  'paths': {
+    'jquery': '//apps.bdimg.com/libs/jquery/2.1.4/jquery.min'
+  }
+});
+
+require(['jquery'], function($) {
+  $(function() {
+    var radStatus,
+      txtTime = $('details input[type="number"]'),
+      txtTitle = $('details input[type="text"]'),
+      txtCode = $('details textarea'),
+      fmButton = $('details form');
+
+    fmButton.submit(function(e) {
+      e.preventDefault();
+      radStatus  = $('details input[type="radio"]:checked');
+
+      require(['10-button'], function(timerButton) {
+        var btn = timerButton({
+          'container': 'div.main',
+          'enabled': radStatus.val() === 'true',
+          'duration': Number(txtTime.val()),
+          'text': txtTitle.val()
+        });
+
+        btn.on('timer-button-click', new Function(txtCode.val()));
+      });
+    });
+  });
+});
