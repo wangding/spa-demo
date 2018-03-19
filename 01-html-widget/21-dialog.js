62f42013e2da9d34dfc2c4acab1af1dd4530349f
wangding <408542507@qq.com>
2018-03-19 19:17:21 +0800

    refactor: 修改了对话框的样式

--- 01-html-widget/21-dialog.js
+++ 01-html-widget/21-dialog.js
@@ -1,21 +1,22 @@
 $(function() {
-  var $Alert = $('#alert');      // alert 弹窗
-  var $Prompt = $('#prompt');    // prompt 弹窗
-  var $Confirm = $('#confirm');  // confirm 弹窗
+  var $btnAlert = $('#alert'),
+    $btnPrompt = $('#prompt'),
+    $btnConfirm = $('#confirm'),
+    $msg = $('#msg');
   
-  $Alert.click(function(){
+  $btnAlert.click(function(){
     alert('JavaScript 是一门伟大的编程语言！');
   });
 
-  $Prompt.click(function(){
+  $btnPrompt.click(function(){
     var id = prompt('请输入你的身份证号：');
     var msg = (id === '') ? '你的身份信息呢？' : '你的身份证号是：' + id;
-    $('.ans').html(msg);
+    $msg.html(msg);
   });
 
-  $Confirm.click(function(){
+  $btnConfirm.click(function(){
     var ans = confirm('你打算考研吗？');
-    var msg = ans ? '研路漫漫，我来陪你' : '去职场丰富经验吧';
-    $('.ans').html(msg);
+    var msg = ans ? '研路漫漫，我来陪你!' : '去职场丰富经验吧!';
+    $msg.html(msg);
   });
 });
