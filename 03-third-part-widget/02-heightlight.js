722d830b1b8fa0530482dd5074c23161af9474d4
wangding <408542507@qq.com>
2019-04-03 16:42:54 +0800

    feat: 增加了高亮代码删除的功能

--- 03-third-part-widget/02-heightlight.js
+++ 03-third-part-widget/02-heightlight.js
@@ -13,7 +13,13 @@ $(function() {
   });
 
   $('input[type="button"]').click(function() {
-    var $code = $('<div><pre class="javascript"></pre></div>'),
+    var $code = $(''
+        + '<div>'
+          + '<pre class="javascript">'
+          + '</pre>'
+          + '<i class="iconfont icon-clipboard" title="复制到剪贴板"></i>'
+          + '<i class="iconfont icon-delete" title="删除"></i>'
+        + '</div>'),
         $txt  = $('.main textarea');
 
     if($txt.val() !== '') {
@@ -21,6 +27,24 @@ $(function() {
       hljs.highlightBlock($code.find('pre').get(0));
       $('.main').append($code);
     }
+
+    var $del = $code.find('i.icon-delete'),
+        $copy = $code.find('i.icon-clipboard');
+
+    $del.click(function() {
+      $code.remove();
+    });
+
+    $copy.click(function() {
+      var $content = $code.find('pre');
+
+      var range = document.createRange();
+      range.selectNode($content[0]);
+      window.getSelection().addRange(range);
+
+      document.execCommand('copy');
+      window.getSelection().removeAllRanges();
+    });
   });
 
   return editor;
