403871beeee89104b62315f33db9ccf674646790
wangding <408542507@qq.com>
2019-04-30 16:41:04 +0800

    fix: 修改历史保存代码

--- 07-storage/02-history.js
+++ 07-storage/02-history.js
@@ -1,26 +1,25 @@
 $(function() {
-  setBtnTitle(getN());
+  var $btn = $('.main input');
 
-  $('.main input').click(function() {
-    setBtnTitle(getN() + 1);
-    history.pushState(null, null, getURL());
+  var n = 0;
+
+  setBtnTitle(n);
+
+  $btn.click(function() {
+    var url = location.origin + location.pathname + '#/' + (++n);
+
+    history.pushState(null, null, url);
+    setBtnTitle(n);
   });
 
   $(window).on('popstate', function() {
-    setBtnTitle(getN());
+    n = (location.href).match(/(\d*)$/)[0];
+
+    n = (n === '' ? 0 : n);
+    setBtnTitle(n);
   });
 
-  function getURL() {
-    var url = window.location.origin + window.location.pathname;
-    return url + '#!' + (getN() + 1);
-  }
-
-  function getN() {
-    var url = window.location.href;
-    return Number(url.match(/(\d*)$/)[0]);
-  }
-
   function setBtnTitle(n) {
-    $('.main input').val('被按了 ' + n + ' 次');
+    $btn.val('被按了 ' + n + ' 次');
   }
 });
